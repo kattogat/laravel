bcfb1d03fa42df8579e1a9a025d217b0f4e7c6a5
NOVA\maria <maria_hav@live.se>
2017-11-21 15:41:41 +0100

    Exercise 4

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -4,14 +4,19 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Customer;
+
 class CustomersController extends Controller
 {
     public function index() {
         return response()->json(Customer::all());
     }
 
-    public function onlyOne() {
-        $id = collect(request()->segments())->last();
-        return response()->json(Customer::find($id));
+    public function onlyOne($id) {
+        $customer = Customer::find($id);
+        if(is_object($customer)) {
+            return response()->json(Customer::find($id));
+        } else {
+            return response()->json(["message" => "Customer not found"], 404);
+        }$customer = Customer::find($id);
     }
 }
